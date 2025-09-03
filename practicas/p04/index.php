d5f0de8a2f5e584e750fa777bd6a6c1640995f96
Israel Ignacio Calderon Paisano <meliodaxx28@gmail.com>
2025-09-03 11:23:28 -0600

    Primer inciso cargado

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -2,33 +2,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Práctica 4</title>
+    <title>Práctica 4 Variables php</title>
 </head>
 <body>
-    <h2>Ejercicio 1</h2>
-    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
-    <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
+    <h2>Manejo de variables en php</h2>
     <?php
-        //AQUI VA MI CÓDIGO PHP
-        $_myvar;
-        $_7var;
-        //myvar;       // Inválida
-        $myvar;
-        $var7;
-        $_element1;
-        //$house*5;     // Invalida
-        
-        echo '<h4>Respuesta:</h4>';   
-    
-        echo '<ul>';
-        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
-        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
-        echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
-        echo '<li>$myvar es válida porque inicia con una letra.</li>';
-        echo '<li>$var7 es válida porque inicia con una letra.</li>';
-        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
-        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
-        echo '</ul>';
-    ?>
+            echo "<h2>Inciso 1. Variables validas</h2>";
+            echo '$_myvar: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php';
+            echo "<br>";
+            echo '$_7var: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php';
+            echo "<br>";
+            echo 'myvar: no es valida, porque no comienza con el signo de $, no sigue la nomenclatura de php';
+            echo "<br>";
+            echo '$myvar: es valida, porque comienza el signo $, sigue la nomenclatura de php';
+            echo "<br>";
+            echo '$var7: es valida, porque comienza con el signo $, sigue la nomenclatura de php';
+            echo "<br>";
+            echo '$_element1: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php';
+	    echo "<br>";
+            echo '$house*5: no es valida, porque tiene un signo *, no sigue la nomenclatura de php';
+        ?>
 </body>
 </html>
