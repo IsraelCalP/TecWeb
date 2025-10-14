1d66fac48a5a5c6990b939a1b0c4c3aeaf9f2f2f
Israel Ignacio Calderon Paisano <meliodaxx28@gmail.com>
2025-10-13 21:02:43 -0600

    Carpeta base para práctica 9 creada

--- practicas/p04/XHTML.php
+++ practicas/p04/XHTML.php
@@ -1,4 +1,4 @@
-
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
@@ -7,31 +7,116 @@
 </head>
 <body>
     <h2>Manejo de variables en php</h2>
-    <h2>Inciso 1. Variables validas</h2>$_myvar: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php<br>$_7var: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php<br>myvar: no es valida, porque no comienza con el signo de $, no sigue la nomenclatura de php<br>$myvar: es valida, porque comienza el signo $, sigue la nomenclatura de php<br>$var7: es valida, porque comienza con el signo $, sigue la nomenclatura de php<br>$_element1: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php<br>$house*5: no es valida, porque tiene un signo *, no sigue la nomenclatura de php    <h2>Inciso 2. Proporcionar valores</h2><h3>a)</h3>Variable $a: ManejadorSQL <br>Variable $b: MySQL <br>Variable $c: ManejadorSQL <br><h3>b) y c)</h3>Variable $a: PHP server <br>Variable $b: PHP server <br>Variable $c: PHP server <br><h3>d)</h3>La variable $a cambia su valor a 'PHP server' y como las variables $b y $c son referencias de $a, toman el mismo valor.    <h2>Inciso 3. Mostrar contenido de variables después de la asignación.</h2>Variable $a: PHP5 <br>Arreglo $z después de agregar $a:<br><pre>Array
-(
-    [0] => PHP5
-)
-</pre>Variable $b: 5a version de PHP <br>Variable $c: 50 <br>Variable $a después de concatenar $b: 5050 <br>Variable $b después de multiplicar por $c: 25502500 <br>Arreglo $z después de cambiar $z[0]:<br><pre>Array
-(
-    [0] => MySQL
-)
-</pre>     <h2>Inciso 4. Mostrar contenido de variables después de la asignación con $GLOBALS</h2>Variable $a: PHP5<br>Arreglo $z:<br><pre>Array
-(
-    [0] => PHP5
-)
-</pre>Variable $b: 5a version de PHP<br>Variable $c: 50<br>Variable $a después de concatenar $b: PHP55a version de PHP<br>Variable $b después de multiplicar por $c: 250<br>Arreglo $z después de cambiar $z[0]:<br><pre>Array
-(
-    [0] => MySQL
-)
-</pre> 
-     <h2>Inciso 5. Dar valor a variables $a, $b, $c</h2>Variable $a: 9e3 <br>Variable $b: 7 <br>Variable $c: 9000 <br><h2>Inciso 6. Compronar valor booleano variables</h2>Variable $a: string(1) "0"
-<br>Variable $b: string(4) "TRUE"
-<br>Variable $c: bool(false)
-<br>Variable $d: bool(true)
-<br>Variable $e: bool(false)
-<br>Variable $f: bool(true)
+    
+    <h3>Inciso 1. Variables validas</h3>
+    <p>
+        $_myvar: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php<br />
+        $_7var: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php<br />
+        myvar: no es valida, porque no comienza con el signo de $, no sigue la nomenclatura de php<br />
+        $myvar: es valida, porque comienza el signo $, sigue la nomenclatura de php<br />
+        $var7: es valida, porque comienza con el signo $, sigue la nomenclatura de php<br />
+        $_element1: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php<br />
+        $house*5: no es valida, porque tiene un signo *, no sigue la nomenclatura de php
+    </p>
+    
+    <h3>Inciso 2. Proporcionar valores</h3>
+    <h4>a)</h4>
+    <p>
+        Variable $a: ManejadorSQL <br />
+        Variable $b: MySQL <br />
+        Variable $c: ManejadorSQL
+    </p>
+    
+    <h4>b) y c)</h4>
+    <p>
+        Variable $a: PHP server <br />
+        Variable $b: PHP server <br />
+        Variable $c: PHP server
+    </p>
 
-    <h2>Transformación de valor booleano de la variable 0 y 0</h2>Variable $c: 0<br>Variable $e: 0    <h2>Inciso 7: Uso de variable $_SERVER</h2>Versión de apache: Apache/2.4.58 (Win64) OpenSSL/3.1.3 PHP/8.2.12<br>Versión de php: 8.2.12
-    Nombre del sistema operativo: WINNT    Idioma de navegador: es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5,es-MX;q=0.4
+    <h4>d)</h4>
+    <p>La variable $a cambia su valor a 'PHP server' y como las variables $b y $c son referencias de $a, toman el mismo valor.</p>
+    
+    <h3>Inciso 3. Mostrar contenido de variables después de la asignación.</h3>
+    <p>Variable $a: PHP5</p>
+    <p>Arreglo $z después de agregar $a:</p>
+    <pre>
+Array
+(
+    [0] => PHP5
+)
+    </pre>
+    <p>
+        Variable $b: 5a version de PHP <br />
+        Variable $c: 50 <br />
+        Variable $a después de concatenar $b: 5050 <br />
+        Variable $b después de multiplicar por $c: 25502500
+    </p>
+    <p>Arreglo $z después de cambiar $z[0]:</p>
+    <pre>
+Array
+(
+    [0] => MySQL
+)
+    </pre>
+    
+    <h3>Inciso 4. Mostrar contenido de variables después de la asignación con $GLOBALS</h3>
+    <p>
+        Variable $a: PHP5<br />
+        Arreglo $z:
+    </p>
+    <pre>
+Array
+(
+    [0] => PHP5
+)
+    </pre>
+    <p>
+        Variable $b: 5a version de PHP<br />
+        Variable $c: 50<br />
+        Variable $a después de concatenar $b: PHP55a version de PHP<br />
+        Variable $b después de multiplicar por $c: 250
+    </p>
+    <p>Arreglo $z después de cambiar $z[0]:</p>
+    <pre>
+Array
+(
+    [0] => MySQL
+)
+    </pre>
+
+    <h3>Inciso 5. Dar valor a variables $a, $b, $c</h3>
+    <p>
+        Variable $a: 9e3 <br />
+        Variable $b: 7 <br />
+        Variable $c: 9000
+    </p>
+
+    <h3>Inciso 6. Comprobar valor booleano variables</h3>
+    <p>
+        Variable $a: string(1) "0"<br />
+        Variable $b: string(4) "TRUE"<br />
+        Variable $c: bool(false)<br />
+        Variable $d: bool(true)<br />
+        Variable $e: bool(false)<br />
+        Variable $f: bool(true)
+    </p>
+    <h4>Transformación de valor booleano de la variable 0 y 0</h4>
+    <p>
+        Variable $c: 0<br />
+        Variable $e: 0
+    </p>
+
+    <h3>Inciso 7: Uso de variable $_SERVER</h3>
+    <p>
+        Versión de apache: Apache/2.4.58 (Win64) OpenSSL/3.1.3 PHP/8.2.12<br />
+        Versión de php: 8.2.12<br />
+        Nombre del sistema operativo: WINNT<br />
+        Idioma de navegador: es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5,es-MX;q=0.4
+    </p>
+  <p>
+    <a href="https://validator.w3.org/check?uri=referer"><img
+      src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
 </body>
 </html>
--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -1,3 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
@@ -6,159 +7,130 @@
 </head>
 <body>
     <h2>Manejo de variables en php</h2>
+
     <?php
-            echo "<h2>Inciso 1. Variables validas</h2>";
-            echo '$_myvar: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php';
-            echo "<br>";
-            echo '$_7var: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php';
-            echo "<br>";
-            echo 'myvar: no es valida, porque no comienza con el signo de $, no sigue la nomenclatura de php';
-            echo "<br>";
-            echo '$myvar: es valida, porque comienza el signo $, sigue la nomenclatura de php';
-            echo "<br>";
-            echo '$var7: es valida, porque comienza con el signo $, sigue la nomenclatura de php';
-            echo "<br>";
-            echo '$_element1: es valida, porque comienza con un guion bajo, sigue la nomenclatura de php';
-	    echo "<br>";
-            echo '$house*5: no es valida, porque tiene un signo *, no sigue la nomenclatura de php';
-        ?>
-    <?php
-       	    echo "<h2>Inciso 2. Proporcionar valores</h2>";
-            echo "<h3>a)</h3>";
-            $a = "ManejadorSQL";
-            $b = 'MySQL';
-            $c = &$a;
-
-            echo "Variable \$a: $a <br>";
-            echo "Variable \$b: $b <br>";
-            echo "Variable \$c: $c <br>";
-
-            echo "<h3>b) y c)</h3>";
-            $a = "PHP server";
-            $b = &$a;
-
-            echo "Variable \$a: $a <br>";
-            echo "Variable \$b: $b <br>";
-            echo "Variable \$c: $c <br>";
-
-            echo "<h3>d)</h3>";
-            echo "La variable \$a cambia su valor a 'PHP server' y como las variables \$b y \$c son referencias de \$a, toman el mismo valor.";
-        ?>
-    <?php
-    	    echo "<h2>Inciso 3. Mostrar contenido de variables después de la asignación.</h2>";
-	    $a = "PHP5";
-    	    echo "Variable \$a: $a <br>";
-
-    	    $z[] = &$a;
-    	    echo "Arreglo \$z después de agregar \$a:<br>";
-    	    echo "<pre>";
-    	    print_r($z);
-    	    echo "</pre>";
-
-            $b = "5a version de PHP";
-            echo "Variable \$b: $b <br>";
-
-            @$c = $b * 10; 
-            echo "Variable \$c: $c <br>";
-
-    	    $a .= $b; 
-            echo "Variable \$a después de concatenar \$b: $a <br>";
-
-            @$b *= $c; 
-            echo "Variable \$b después de multiplicar por \$c: $b <br>";
-
-            $z[0] = "MySQL";
-            echo "Arreglo \$z después de cambiar \$z[0]:<br>";
-            echo "<pre>";
-            print_r($z);
-            echo "</pre>";
-
-            unset($a, $b, $c, $z);
-    ?>
-     <?php
-    	    echo "<h2>Inciso 4. Mostrar contenido de variables después de la asignación con \$GLOBALS</h2>";
-
-    	    $GLOBALS['a'] = "PHP5";
-	        echo "Variable \$a: " . $GLOBALS['a'] . "<br>";
-	        $GLOBALS['z'][] = &$GLOBALS['a'];
-	        echo "Arreglo \$z:<br>";
-	        echo "<pre>";
-	        print_r($GLOBALS['z']);
-	        echo "</pre>";
-
-	        $GLOBALS['b'] = "5a version de PHP";
-	        echo "Variable \$b: " . $GLOBALS['b'] . "<br>";
-
-	        @$GLOBALS['c'] = $GLOBALS['b'] * 10;
-	        echo "Variable \$c: " . $GLOBALS['c'] . "<br>";
-
-	        $GLOBALS['a'] .= $GLOBALS['b'];
-	        echo "Variable \$a después de concatenar \$b: " . $GLOBALS['a'] . "<br>";
-
-	        @$GLOBALS['b'] *= $GLOBALS['c'];
-	        echo "Variable \$b después de multiplicar por \$c: " . $GLOBALS['b'] . "<br>";
-
-	        $GLOBALS['z'][0] = "MySQL";
-	        echo "Arreglo \$z después de cambiar \$z[0]:<br>";
-	        echo "<pre>";
-	        print_r($GLOBALS['z']);
-	        echo "</pre>";
-
-	        unset($GLOBALS['a'], $GLOBALS['b'], $GLOBALS['c'], $GLOBALS['z']);
-    ?> 
-     <?php
-	        echo "<h2>Inciso 5. Dar valor a variables \$a, \$b, \$c</h2>";
-
-	        $a = "7 personas";
-	        $b = (int) $a;
-	        $a = "9e3";
-	        $c = (float) $a;
-
-	        echo "Variable \$a: $a <br>";
-	        echo "Variable \$b: $b <br>";
-	        echo "Variable \$c: $c <br>";
-    ?>
-<?php
-            echo "<h2>Inciso 6. Compronar valor booleano variables</h2>";
-            $a = "0";
-            $b = "TRUE";
-            $c = FALSE;
-            $d = ($a OR $b);
-            $e = ($a AND $c);
-            $f = ($a XOR $b);
-
-            echo "Variable \$a: ", var_dump($a);
-            echo "<br>";
-            echo "Variable \$b: ", var_dump($b);
-            echo "<br>";
-            echo "Variable \$c: ", var_dump($c);
-            echo "<br>";
-            echo "Variable \$d: ", var_dump($d);
-            echo "<br>";
-            echo "Variable \$e: ", var_dump($e);
-            echo "<br>";
-            echo "Variable \$f: ", var_dump($f);
+        echo "<h3>Inciso 1. Variables válidas</h3>";
+        echo "<p>";
+        echo '$_myvar: es válida, porque comienza con un guion bajo, sigue la nomenclatura de PHP.<br />';
+        echo '$_7var: es válida, porque comienza con un guion bajo, sigue la nomenclatura de PHP.<br />';
+        echo 'myvar: no es válida, porque no comienza con el signo de $, no sigue la nomenclatura de PHP.<br />';
+        echo '$myvar: es válida, porque comienza con el signo $, sigue la nomenclatura de PHP.<br />';
+        echo '$var7: es válida, porque comienza con el signo $, sigue la nomenclatura de PHP.<br />';
+        echo '$_element1: es válida, porque comienza con un guion bajo, sigue la nomenclatura de PHP.<br />';
+        echo '$house*5: no es válida, porque tiene un signo *, no sigue la nomenclatura de PHP.';
+        echo "</p>";
     ?>
 
     <?php
-            echo "<h2>Transformación de valor booleano de la variable " . (int)$c . " y " . (int)$e . "</h2>";
-            echo "Variable \$c: " . (int)$c;
-            echo "<br>";
-            echo "Variable \$e: " . (int)$e;
-    ?>
-    <?php
-            echo "<h2>Inciso 7: Uso de variable \$_SERVER</h2>";
-            echo "Versión de apache: " . $_SERVER['SERVER_SOFTWARE'];
-            echo "<br>";
-            echo "Versión de php: " . phpversion();
+        echo "<h3>Inciso 2. Proporcionar valores</h3>";
+        echo "<h4>a)</h4>";
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+        echo "<p>Variable \$a: $a <br />Variable \$b: $b <br />Variable \$c: $c</p>";
+
+        echo "<h4>b) y c)</h4>";
+        $a = "PHP server";
+        $b = &$a;
+        echo "<p>Variable \$a: $a <br />Variable \$b: $b <br />Variable \$c: $c</p>";
+
+        echo "<h4>d)</h4>";
+        echo "<p>La variable \$a cambia su valor a 'PHP server' y como las variables \$b y \$c son referencias de \$a, toman el mismo valor.</p>";
     ?>
 
     <?php
-            echo "Nombre del sistema operativo: " . PHP_OS;
-    ?>
-    <?php
-            echo "Idioma de navegador: " . $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+        echo "<h3>Inciso 3. Mostrar contenido de variables después de la asignación.</h3>";
+        $a = "PHP5";
+        echo "<p>Variable \$a: $a</p>";
+
+        $z[] = &$a;
+        echo "<p>Arreglo \$z después de agregar \$a:</p><pre>";
+        print_r($z);
+        echo "</pre>";
+
+        $b = "5a version de PHP";
+        @$c = $b * 10;
+        $a .= $b;
+        @$b *= $c;
+        echo "<p>Variable \$b: 5a version de PHP <br />Variable \$c: 50 <br />Variable \$a después de concatenar \$b: 5050 <br />Variable \$b después de multiplicar por \$c: 25502500</p>";
+
+        $z[0] = "MySQL";
+        echo "<p>Arreglo \$z después de cambiar \$z[0]:</p><pre>";
+        print_r($z);
+        echo "</pre>";
+
+        unset($a, $b, $c, $z);
     ?>
 
+    <?php
+        echo "<h3>Inciso 4. Mostrar contenido de variables después de la asignación con \$GLOBALS</h3>";
+        $GLOBALS['a'] = "PHP5";
+        echo "<p>Variable \$a: " . $GLOBALS['a'] . "</p>";
+
+        $GLOBALS['z'][] = &$GLOBALS['a'];
+        echo "<p>Arreglo \$z:</p><pre>";
+        print_r($GLOBALS['z']);
+        echo "</pre>";
+
+        $GLOBALS['b'] = "5a version de PHP";
+        @$GLOBALS['c'] = $GLOBALS['b'] * 10;
+        $GLOBALS['a'] .= $GLOBALS['b'];
+        @$GLOBALS['b'] *= $GLOBALS['c'];
+
+        echo "<p>Variable \$b: 5a version de PHP<br />Variable \$c: 50<br />Variable \$a después de concatenar \$b: PHP55a version de PHP<br />Variable \$b después de multiplicar por \$c: 250</p>";
+
+        $GLOBALS['z'][0] = "MySQL";
+        echo "<p>Arreglo \$z después de cambiar \$z[0]:</p><pre>";
+        print_r($GLOBALS['z']);
+        echo "</pre>";
+
+        unset($GLOBALS['a'], $GLOBALS['b'], $GLOBALS['c'], $GLOBALS['z']);
+    ?>
+
+    <?php
+        echo "<h3>Inciso 5. Dar valor a variables \$a, \$b, \$c</h3>";
+        $a = "7 personas";
+        $b = (int) $a;
+        $a = "9e3";
+        $c = (float) $a;
+        echo "<p>Variable \$a: $a <br />Variable \$b: $b <br />Variable \$c: $c</p>";
+    ?>
+
+    <?php
+        echo "<h3>Inciso 6. Comprobar valor booleano de variables</h3>";
+        $a = "0";
+        $b = "TRUE";
+        $c = FALSE;
+        $d = ($a OR $b);
+        $e = ($a AND $c);
+        $f = ($a XOR $b);
+
+        echo "<p>Variable \$a:</p><pre>"; var_dump($a); echo "</pre>";
+        echo "<p>Variable \$b:</p><pre>"; var_dump($b); echo "</pre>";
+        echo "<p>Variable \$c:</p><pre>"; var_dump($c); echo "</pre>";
+        echo "<p>Variable \$d:</p><pre>"; var_dump($d); echo "</pre>";
+        echo "<p>Variable \$e:</p><pre>"; var_dump($e); echo "</pre>";
+        echo "<p>Variable \$f:</p><pre>"; var_dump($f); echo "</pre>";
+    ?>
+
+    <?php
+        echo "<h4>Transformación de valor booleano de la variable</h4>";
+        echo "<p>Variable \$c: " . (int)$c . "<br />Variable \$e: " . (int)$e . "</p>";
+    ?>
+
+    <?php
+        echo "<h3>Inciso 7: Uso de variable \$_SERVER</h3>";
+        echo "<p>Versión de apache: " . $_SERVER['SERVER_SOFTWARE'] . "<br />";
+        echo "Versión de php: " . phpversion() . "<br />";
+        echo "Nombre del sistema operativo: " . PHP_OS . "<br />";
+        echo "Idioma de navegador: " . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "</p>";
+    ?>
+
+    <p>
+        <a href="https://validator.w3.org/check?uri=referer">
+            <img src="https://www.w3.org/Icons/valid-xhtml11"
+                 alt="Valid XHTML 1.1" height="31" width="88" />
+        </a>
+    </p>
 </body>
 </html>
