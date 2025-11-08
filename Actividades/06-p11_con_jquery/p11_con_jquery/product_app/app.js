17d959d44f393abf7d9cc301073a735242a941a4
Israel Ignacio Calderon Paisano <meliodaxx28@gmail.com>
2025-11-07 20:59:23 -0600

    Modificación de botón modificar producto al seleccionar uno existente

--- Actividades/06-p11_con_jquery/p11_con_jquery/product_app/app.js
+++ Actividades/06-p11_con_jquery/p11_con_jquery/product_app/app.js
@@ -155,6 +155,9 @@ $(document).ready(function(){
             listarProductos();
             // SE REGRESA LA BANDERA DE EDICIÓN A false
             edit = false;
+
+            // ===== CÓDIGO AÑADIDO PARA RESETEAR EL BOTÓN =====
+            $('button.btn-primary').text("Agregar Producto");
         });
     });
 
@@ -190,6 +193,9 @@ $(document).ready(function(){
             
             // SE PONE LA BANDERA DE EDICIÓN EN true
             edit = true;
+
+            // ===== CÓDIGO SOLICITADO AÑADIDO =====
+            $('button.btn-primary').text("Modificar Producto");
         });
         e.preventDefault();
     });    
