f211255c237a81ee41886271ef99758747bf5a6f
Israel Ignacio Calderon Paisano <meliodaxx28@gmail.com>
2025-11-12 13:29:58 -0600

    DataBase codificado y Products creado además el primer database ha sido comentado

--- Actividades/a07/p11_con_jquery/product_app/backend/database.php
+++ Actividades/a07/p11_con_jquery/product_app/backend/database.php
@@ -1,15 +1,15 @@
 <?php
-    $conexion = @mysqli_connect(
-        'localhost',
-        'root',
-        'Isra2818',
-        'marketzone'
-    );
+    //$conexion = @mysqli_connect(
+      //  'localhost',
+        //'root',
+        //'Isra2818',
+        //'marketzone'
+    //);
 
     /**
      * NOTA: si la conexión falló $conexion contendrá false
      **/
-    if(!$conexion) {
-        die('¡Base de datos NO conextada!');
-    }
+    //if(!$conexion) {
+        //die('¡Base de datos NO conextada!');
+    //}
 ?>
