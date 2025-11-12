2d362bb7cad10b5de69e3982e85cc3fdc9ba7fce
Israel Ignacio Calderon Paisano <meliodaxx28@gmail.com>
2025-11-12 14:01:08 -0600

    product single corregido

--- Actividades/a07/p11_con_jquery/product_app/backend/product-single.php
+++ Actividades/a07/p11_con_jquery/product_app/backend/product-single.php
@@ -1,7 +1,9 @@
 <?php
-    include_once __DIR__.'/database.php';
+use TECWEB\MYAPI\Products;
+require_once __DIR__ . '/myapi/Products.php';
+header('Content-Type: application/json');
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    /*// SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
 
     if( isset($_POST['id']) ) {
@@ -25,5 +27,20 @@
     }
 
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+    echo json_encode($data, JSON_PRETTY_PRINT); */
+
+    try {
+    // 3. Crear instancia
+    $productos = new Products("marketzone", "root", "Isra2818");
+
+    // 4. Invocar al método single() pasándole los datos del POST
+    $productos->single($_POST);
+
+    // 5. Devolver respuesta JSON
+    echo $productos->getData();
+
+} catch (Exception $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
+
 ?>
